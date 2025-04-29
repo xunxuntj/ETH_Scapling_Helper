2cd7dc8c69de07d1876fe7b35ef9d3b7fb42592e
xunxuntj <xunxuntj@gmail.com>
2025-04-29 03:41:32 +0000

    feat: add the table structure to the SignalHistory component

--- frontend/components/SignalHistory.js
+++ frontend/components/SignalHistory.js
@@ -1,5 +1,24 @@
 function SignalHistory() {
-  return null;
+  return (
+    <div>
+      <h2>Signal History</h2>
+      <div> {/* Main table container */}
+        <div> {/* Header Row */}
+          <div>Timestamp</div>
+          <div>Signal Name</div>
+          <div>Signal Value</div>
+          <div>Result</div>
+        </div>
+        <div> {/* Example Row */}
+          <div>2024-10-27 10:00:00</div>
+          <div>MACD</div>
+          <div>Bullish</div>
+          <div>Success</div>
+        </div>
+         {/* Add more rows here as needed */}
+      </div>
+    </div>
+  );
 }
 
 export default SignalHistory;
